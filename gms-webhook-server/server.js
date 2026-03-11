1858efa3c3a797e0e282f032035a69a00f828895
Lordwill1 <messagelordwill@gmail.com>
2026-03-11 21:26:51 +0100

    Added debug directions

--- gms-webhook-server/server.js
+++ gms-webhook-server/server.js
@@ -149,8 +149,7 @@ app.get('/', (req, res) => {
                     <li><code>GET /callback-url</code> - Get callback URL in JSON format</li>
                     <li><code>GET /test-webhook</code> - Test page for sending sample webhooks</li>
                     <li><code>GET /health</code> - Health check endpoint</li>
-                    <li><code>GET /debug-directions</code> - Debug direction values</li>
-                    <li><code>GET /debug-directions</code> <a href="/debug/directions" class="debug-btn"> - Debug direction values</a></li>
+                    <li><code>GET /debug-directions</code> - <a href="/debug/directions" class="debug-btn">Debug direction values</a></li>
                 </ul>
 
                 <h2>GMS Webhook Formats Accepted:</h2>
